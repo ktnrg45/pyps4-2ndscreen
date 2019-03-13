2df21a70672445c877818029e374986086e634a9
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-13 12:44:35 -0700

    Update errors.py

--- pyps4-homeassistant/errors.py
+++ pyps4-homeassistant/errors.py
@@ -1,6 +1,15 @@
+"""Exceptions Definitions."""
+
 class NotReady(Exception):
+    """PS4 no connection."""
+    pass
+
+
+class LoginFailed(Exception):
+    """PS4 Failed Login."""
     pass
 
 
 class UnknownButton(Exception):
+    """Button not valid."""
     pass
