73a702a7e4ce5b074019842969f81e31c0cb95e0
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-13 12:43:35 -0700

    reformat

--- pyps4-homeassistant/media_art.py
+++ pyps4-homeassistant/media_art.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-def get_ps_store_url(self, title, region):
+"""Media Art Functions."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def get_ps_store_url(title, region):
     """Get URL for title search in PS Store."""
     import urllib
     import re
@@ -29,13 +35,14 @@ def get_ps_store_url(self, title, region):
     url = [_url, headers]
     return url
 
-def get_ps_store_data(self, title, title_id, region, url=None):
+
+def get_ps_store_data(title, title_id, region, url=None):
     """Store cover art from PS store in games map."""
     import requests
     import re
 
     if url is None:
-        url = self.get_ps_store_url(title, region)
+        url = get_ps_store_url(title, region)
     req = None
     match_id = {}
     match_title = {}
@@ -46,7 +53,7 @@ def get_ps_store_data(self, title, title_id, region, url=None):
         result = req.json()['included']
         if not result:
             title = title.split(' ')
-            return self.get_ps_store_data(title[0], title_id, region, url=None)
+            return get_ps_store_data(title[0], title_id, region, url=None)
     except requests.exceptions.HTTPError as warning:
         _LOGGER.warning("PS cover art HTTP error, %s", warning)
         return
@@ -62,26 +69,26 @@ def get_ps_store_data(self, title, title_id, region, url=None):
             has_parent = False
 
             # Set each item as Game object
-            game = self._game(item)
+            game = _game(item)
 
             # Get Parent attr
             if 'parent' in game:
                 if game['parent'] is not None:
                     has_parent = True
                     parent = game['parent']
-                    parent_id = self._parse_id(parent['id'])
+                    parent_id = _parse_id(parent['id'])
                     parent_title = parent['name']
                     parent_art = parent['url']
                     if parent_id == title_id:
                         return parent_title, parent_art
 
-            if self._is_game_type(game, game_type):
-                parse_id = self._parse_id(game['default-sku-id'])
+            if _is_game_type(game, game_type):
+                parse_id = _parse_id(game['default-sku-id'])
                 title_parse = game['name']
 
                 # If passed SKU matches object SKU
                 if parse_id == title_id:
-                    cover_art = self._get_cover(game)
+                    cover_art = _get_cover(game)
                     if cover_art is not None:
 
                         # If true likely a bundle, dlc, deluxe edition
@@ -89,32 +96,35 @@ def get_ps_store_data(self, title, title_id, region, url=None):
                             match_id.update({title_parse: cover_art})
 
                         # Most likely the intended item so return
-                        if title.upper() == self._format_title(title_parse):
+                        if title.upper() == _format_title(title_parse):
                             return title_parse, cover_art
 
                 # Last resort filter if SKU wrong, but title matches.
-                elif title.upper() == self._format_title(title_parse):
-                    cover_art = self._get_cover(game)
+                elif title.upper() == _format_title(title_parse):
+                    cover_art = _get_cover(game)
                     if cover_art is not None:
                         if has_parent is False:
                             match_title.update({title_parse: cover_art})
 
-    return self._get_similar(title, match_id, match_title)
+    return _get_similar(title, match_id, match_title)
 
-def _game(self, item):
+
+def _game(item):
     """Create game object."""
     if 'attributes' in item:
         game = item['attributes']
         return game
 
-def _is_game_type(self, game, game_type):
+
+def _is_game_type(game, game_type):
     """Check if item is a game and has SKU."""
     if 'game-content-type' in game and \
        game['game-content-type'] == game_type:
         if 'default-sku-id' in game:
             return True
 
-def _parse_id(self, _id):
+
+def _parse_id(_id):
     """Parse SKU to simplified ID."""
     full_id = _id
     full_id = full_id.split("-")
@@ -123,7 +133,8 @@ def _parse_id(self, _id):
     parse_id = full_id[0]
     return parse_id
 
-def _format_title(self, title):
+
+def _format_title(title):
     """Format Title."""
     import re
 
@@ -131,7 +142,8 @@ def _format_title(self, title):
     title = title.upper()
     return title
 
-def _get_cover(self, game):
+
+def _get_cover(game):
     """Get cover art."""
     if 'thumbnail-url-base' in game:
         cover = 'thumbnail-url-base'
@@ -139,12 +151,13 @@ def _get_cover(self, game):
         return cover_art
     return
 
-def _get_similar(self, title, match_id, match_title):
+
+def _get_similar(title, match_id, match_title):
     """Return similar title."""
     if match_id:
         _LOGGER.info("Found similar titles: %s", match_id)
         for _title, url in match_id.items():
-            if title.upper() in self._format_title(_title):
+            if title.upper() in _format_title(_title):
                 _LOGGER.info("Using similar title: %s", _title)
                 cover_art = url
                 return _title, cover_art
