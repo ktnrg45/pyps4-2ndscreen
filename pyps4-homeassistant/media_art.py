d408c28d6cb01d88179b2c4e88967fe7ffcef543
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-14 00:06:28 -0700

    Update media_art.py

--- pyps4-homeassistant/media_art.py
+++ pyps4-homeassistant/media_art.py
@@ -5,18 +5,10 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 
-def get_ps_store_url(title, region):
+def get_ps_store_url(title, region, reformat=False):
     """Get URL for title search in PS Store."""
     import urllib
     import re
-
-    regions = {'R1': 'US', 'R2': 'GB', 'R3': 'HK', 'R4': 'AU', 'R5': 'IN'}
-
-    if region not in regions:
-        _LOGGER.error('Region: %s is not valid', region)
-        return
-    else:
-        region = regions[region]
 
     headers = {
         'User-Agent':
@@ -26,17 +18,17 @@ def get_ps_store_url(title, region):
     }
 
     if title is not None:
-        title = re.sub('[^A-Za-z0-9]+', ' ', title)
+        if reformat is True:
+            title = re.sub('[^A-Za-z0-9]+', ' ', title)
         title = urllib.parse.quote(title.encode('utf-8'))
         _url = 'https://store.playstation.com/'\
-            'valkyrie-api/en/{0}/19/faceted-search/'\
+            'valkyrie-api/{0}/19/faceted-search/'\
             '{1}?query={1}&platform=ps4'.format(region, title)
 
     url = [_url, headers]
     return url
 
-
-def get_ps_store_data(title, title_id, region, url=None):
+def get_ps_store_data(title, title_id, region, url=None, reformat=False):
     """Store cover art from PS store in games map."""
     import requests
     import re
@@ -46,14 +38,18 @@ def get_ps_store_data(title, title_id, region, url=None):
     req = None
     match_id = {}
     match_title = {}
-    title = re.sub('[^A-Za-z0-9]+', ' ', title)
+    if reformat is True:
+        title = re.sub('[^A-Za-z0-9]+', ' ', title)
     type_list = ['Full Game', 'Game', 'PSN Game', 'Bundle', 'App']
     try:
         req = requests.get(url[0], headers=url[1])
         result = req.json()['included']
         if not result:
             title = title.split(' ')
-            return get_ps_store_data(title[0], title_id, region, url=None)
+            title = title[0]
+            url = get_ps_store_url(title, region)
+            req = requests.get(url[0], headers=url[1])
+            result = req.json()['included']
     except requests.exceptions.HTTPError as warning:
         _LOGGER.warning("PS cover art HTTP error, %s", warning)
         return
@@ -78,8 +74,16 @@ def get_ps_store_data(title, title_id, region, url=None):
                     parent = game['parent']
                     parent_id = _parse_id(parent['id'])
                     parent_title = parent['name']
+                    parent_title = _format_title(parent_title, reformat)
                     parent_art = parent['url']
                     if parent_id == title_id:
+                        _LOGGER.debug("Parent ID Match")
+                        return parent_title, parent_art
+                    if title.upper() == parent_title:
+                        _LOGGER.debug("Parent Title Match")
+                        return parent_title, parent_art
+                    if title.upper() in parent_title:
+                        _LOGGER.debug("Parent Similar Title Match")
                         return parent_title, parent_art
 
             if _is_game_type(game, game_type):
@@ -96,17 +100,23 @@ def get_ps_store_data(title, title_id, region, url=None):
                             match_id.update({title_parse: cover_art})
 
                         # Most likely the intended item so return
-                        if title.upper() == _format_title(title_parse):
+                        if title.upper() == _format_title(title_parse,
+                                                          reformat):
+                            _LOGGER.debug("Direct Match")
                             return title_parse, cover_art
 
                 # Last resort filter if SKU wrong, but title matches.
-                elif title.upper() == _format_title(title_parse):
+                elif title.upper() == _format_title(title_parse, reformat):
                     cover_art = _get_cover(game)
                     if cover_art is not None:
                         if has_parent is False:
                             match_title.update({title_parse: cover_art})
 
-    return _get_similar(title, match_id, match_title)
+    s_title, s_art = _get_similar(title, match_id, match_title, reformat)
+    if s_title is None or s_art is None:
+        if reformat is False:
+            return get_ps_store_data(TITLE, TITLE_ID, region, reformat=True)
+    return s_title, s_art
 
 
 def _game(item):
@@ -126,19 +136,23 @@ def _is_game_type(game, game_type):
 
 def _parse_id(_id):
     """Parse SKU to simplified ID."""
-    full_id = _id
-    full_id = full_id.split("-")
-    full_id = full_id[1]
-    full_id = full_id.split("_")
-    parse_id = full_id[0]
+    try:
+        full_id = _id
+        full_id = full_id.split("-")
+        full_id = full_id[1]
+        full_id = full_id.split("_")
+        parse_id = full_id[0]
+    except IndexError:
+        parse_id = "None"
     return parse_id
 
 
-def _format_title(title):
+def _format_title(title, reformat):
     """Format Title."""
     import re
 
-    title = re.sub('[^A-Za-z0-9]+', ' ', title)
+    if reformat is True:
+        title = re.sub('[^A-Za-z0-9]+', ' ', title)
     title = title.upper()
     return title
 
@@ -152,20 +166,17 @@ def _get_cover(game):
     return
 
 
-def _get_similar(title, match_id, match_title):
+def _get_similar(title, match_id, match_title, reformat):
     """Return similar title."""
     if match_id:
-        _LOGGER.info("Found similar titles: %s", match_id)
         for _title, url in match_id.items():
-            if title.upper() in _format_title(_title):
-                _LOGGER.info("Using similar title: %s", _title)
+            if title.upper() in _format_title(_title, reformat):
                 cover_art = url
+                _LOGGER.debug("Similar Title Match")
                 return _title, cover_art
     elif match_title:
-        _LOGGER.warning(
-            "Found matching titles with incorrect SKU: %s", match_title)
         for _title, url in match_title.items():
-                _LOGGER.warning("Using matching title: %s", _title)
                 cover_art = url
+                _LOGGER.debug("Wrong ID Match")
                 return _title, cover_art
     return None, None
