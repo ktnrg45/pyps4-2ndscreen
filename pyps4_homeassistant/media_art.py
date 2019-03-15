208b70c3f7bcd01cf7a457c873a78cda431af568
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-15 16:04:08 -0700

    fix url

--- pyps4_homeassistant/media_art.py
+++ pyps4_homeassistant/media_art.py
@@ -110,7 +110,7 @@ def get_ps_store_data(title, title_id, region, url=None, reformat=False):
                     parent_id = _parse_id(parent['id'])
                     parent_title = parent['name']
                     parent_title_f = _format_title(parent_title, reformat)
-                    parent_art = str.format(parent['url'], '/image')
+                    parent_art = "{}/image".format(parent['url'])
                     if parent_id == title_id:
                         _LOGGER.debug("Parent ID Match")
                         return parent_title, parent_art
