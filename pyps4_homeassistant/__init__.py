fa587af6f34c6d0cf13bdce552f09de31b3dee7f
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-15 15:26:20 -0700

    Update __init__.py

--- pyps4_homeassistant/__init__.py
+++ pyps4_homeassistant/__init__.py
@@ -3,6 +3,6 @@ from .connection import *
 from .ddp import *
 from .errors import *
 from .ps4 import *
-from .helper import *
+from .helpers import *
 from .media_art import *
 from .const import *
