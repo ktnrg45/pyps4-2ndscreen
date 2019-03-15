b7ced14827028564613ce049a7427b8f069d3973
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-15 03:45:09 -0700

    Delete const.py

--- const.py
+++
@@ -1,6 +0,0 @@
-MAJOR_VERSION = 0
-MINOR_VERSION = 5
-PATCH_VERSION = 0
-__short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
-__version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
-REQUIRED_PYTHON_VER = (3, 5, 3)
