6e3d945aa2ea8bb37ecb5d8871c9cd842f6c21ed
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-15 03:51:12 -0700

    Create const.py

---
+++ pyps4_homeassistant/const.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Constants for pyps4-homeassistant."""
+MAJOR_VERSION = 0
+MINOR_VERSION = 5
+PATCH_VERSION = 0
+REQUIRED_PYTHON_VER = (3, 5, 3)
+
+__short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
+__version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
+
+__name__ = 'pyps4_homeassistant'
