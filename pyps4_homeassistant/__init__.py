b303cc049a81d9b3131bb78914d00de897ee35c5
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-17 11:26:48 -0700

    Added credential.py

--- pyps4_homeassistant/__init__.py
+++ pyps4_homeassistant/__init__.py
@@ -3,6 +3,7 @@ from .connection import *
 from .ddp import *
 from .errors import *
 from .ps4 import *
+from .credential import *
 from .helpers import *
 from .media_art import *
 from .const import *
