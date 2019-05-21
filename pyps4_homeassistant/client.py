2e6357277aba77505b0880e8640ff7b0e857b26f
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-05-21 12:19:07 -0700

    Update client.py

--- pyps4_homeassistant/client.py
+++ pyps4_homeassistant/client.py
@@ -3,8 +3,12 @@
 import logging
 import threading
 import time
+import queue
 
 _LOGGER = logging.getLogger(__name__)
+
+DEFAULT_INTERVAL = 3
+DEFAULT_INTERVAL_KEEP_ALIVE = 30
 
 
 class PS4Client():
@@ -13,6 +17,7 @@ class PS4Client():
     def __init__(self):
         """Init."""
         self.listeners = {}
+        self.started = []
 
     def start_all(self):
         """Start all listeners."""
@@ -34,59 +39,34 @@ class PS4Client():
     def start_listener(self, ps4):
         """Start listener thread."""
         _LOGGER.debug("Starting listener @ %s", ps4._host)
+        self.started.append(self.listeners[ps4])
         self.listeners[ps4].start()
 
     def stop_listener(self, ps4):
         """Stop a Listener."""
         if ps4 in self.listeners:
             self.listeners[ps4].stop.set()
+            self.started.remove(self.listeners[ps4])
 
     def remove_listener(self, ps4):
         """Stop and Remove a Listener."""
         self.stop_listener(ps4)
         self.listeners.pop(ps4)
 
-    def _connect_ps4(self, ps4):
-        """Try connecting ps4."""
-        from .errors import NotReady
-
-        if ps4 in self.listeners:
-            if self.listeners[ps4].ps4.connected:
-                _LOGGER.debug("PS4 @ %s already connected", ps4._host)
-            else:
-                active = self._get_active()
-                if active is not None:
-                    active.ps4.close()
-                    try:
-                        self.listeners[ps4].ps4.open()
-                    except NotReady:
-                        pass
-                    else:
-                        _LOGGER.warning("PS4 Connected @ %s", ps4._host)
-                        self.listeners[ps4].connected.set()
-        else:
-            _LOGGER.error("Client has no PS4 Listeners")
-
-    def schedule_task(self, ps4, task, connect=True):
+    def schedule_task(self, ps4, task, *args):
         """Schedule socket task in thread."""
-        self.listeners[ps4].block_status.set()
-        # _task = {task: {}}
-
-        if not connect:
-            self.listeners[ps4].task_no_connect = task
-        else:
-            self._connect_ps4(ps4)
-            if ps4.connected:
-                # if args:
-                #     _task[task][args] = args
-                # if kwargs:
-                #     _task[task][kwargs] = kwargs
-                self.listeners[ps4].task = task
+        listener = self.listeners[ps4]
+        queue = listener.queue
+        if args:
+            task = (task, *args)
+        queue.put(task)
 
     def add_ps4(self, ps4):
         """Add PS4 to listeners."""
         if ps4 not in self.listeners:
             self.listeners[ps4] = Listener(client=self, ps4=ps4)
+        else:
+            _LOGGER.warning("Listener already exists")
 
     def add_callback(self, ps4, callback):
         """Add Callback for listener."""
@@ -109,88 +89,82 @@ class Listener(threading.Thread):
         super().__init__()
         self.client = client
         self.ps4 = ps4
-        self.interval = 3
-        self.interval_keep_alive = 30
+        self.interval = DEFAULT_INTERVAL
+        self.interval_keep_alive = DEFAULT_INTERVAL_KEEP_ALIVE
         self.last_poll = 0
         self.last_keep_alive = 0
         self.callbacks = []
-        self.task = None
-        self.task_no_connect = None
+        self.running = False
+        self._status = None
+        self.queue = queue.Queue()
 
         self.stop = threading.Event()
-        self.polling = threading.Event()
-        self.waiting = threading.Event()
-        self.block_status = threading.Event()
-        self.connected = threading.Event()
+        self.block = threading.Event()
 
     def run(self):
         """Run Listener."""
-        if self.ps4.connected:
-            self.connected.set()
-        else:
-            self.connected.clear()
-
         while not self.stop.is_set():
-            if not self.block_status.is_set():
-                self.schedule_status()
-            elif not self.polling.is_set():
-                self.schedule_task()
+            self.running = True
+            while not self.queue.empty():
+                if not self.block.is_set():
+                    # Block queue.
+                    self.block.set()
+                    item = self.queue.get()
 
-    def schedule_task(self):
-        """Schedule a Task."""
-        # if not self.task:
-        #     task = self.task.keys()
-        #     assert len(task) == 1, "Task dict should only have 1 key"
+                    # If args are passed in.
+                    if isinstance(item, tuple):
+                        args = item[1:]
+                        item = item[0]
+                        result = item(*args)
+                    else:
+                        result = item()
 
-        #     if self.task[task]:
-        #         for value in self.task.values():
-        if self.task_no_connect is not None:
-            self.task_no_connect()
-        else:
-            self.connected.wait()
-            self.task()
-        self.task = None
-        self.task_no_connect = None
-        self.block_status.clear()
-
-    def schedule_status(self):
-        """Schedule Status Update."""
-        if self.waiting.is_set():
-            if not self._should_poll(self.last_poll, self.interval):
-                return
-            self.waiting.clear()
-
-        if not self.polling.is_set():
-            if not self.waiting.is_set():
-                self.polling.set()
-
-                # Poll PS4 for Status
-                self.ps4.get_status()
-
-                # If connected send keep alive msg at intervals.
-                if self.ps4.connected:
-                    if self._should_poll(self.last_keep_alive,
-                                         self.interval_keep_alive):
-                        self.ps4.send_status()
+                    if item == self.ps4.get_status:
+                        self._check_status(result)
+                        self.last_poll = time.time()
+                    elif item == self.ps4.send_status:
                         self.last_keep_alive = time.time()
 
-                self.last_poll = time.time()
-                self.waiting.set()
-                self.polling.clear()
-                self._notify_callbacks()
+                    # Reset intervals for status.
+                    else:
+                        self.last_poll = time.time()
+                        self.last_keep_alive = time.time()
+
+                    # Unblock queue.
+                    self.block.clear()
+
+            # Call when queue is empty.
+            # Queue status update.
+            if self._should_poll(self.last_poll, self.interval):
+                self.queue.put(self.ps4.get_status, False)
+
+            # Queue Keep Alive.
+            if self.ps4.connected:
+                if self._should_poll(self.last_keep_alive,
+                                     self.interval_keep_alive):
+                    self.queue.put(self.ps4.send_status, False)
+
+        # If stopping.
+        self.queue.join()
+        self.running = False
+        _LOGGER.debug("Stopping Listener @ %s", self.ps4._host)
 
     def _should_poll(self, last, interval):
+        """Poll timer."""
         time_since = time.time() - last
         if time_since > interval:
             return True
         return False
 
     def _notify_callbacks(self):
+        """Call all callbacks."""
         if self.callbacks:
             for callback in self.callbacks:
                 callback()
 
-    @property
-    def is_connected(self):
-        """Return True if connected with TCP."""
-        return self.ps4.connected
+    def _check_status(self, status):
+        if status != self._status:
+            _LOGGER.debug(
+                "PS4 @ %s: Status changed to %s", self.ps4._host, status)
+            self._status = status
+            self._notify_callbacks()
