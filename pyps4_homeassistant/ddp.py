44361f1ad2e79bf191fba95ecf30100f97678dd3
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-07-09 08:29:47 -0700

    Rename close func

--- pyps4_homeassistant/ddp.py
+++ pyps4_homeassistant/ddp.py
@@ -57,8 +57,7 @@ class DDPProtocol(asyncio.DatagramProtocol):
             if not ps4.unreachable:
                 _LOGGER.info("PS4 @ %s is unreachable", ps4.host)
                 ps4.unreachable = True
-                # Close TCP Connection
-                ps4.close()
+                ps4._close()  # noqa: pylint: disable=protected-access
             ps4.status = None
             if ps4.host in self.callbacks:
                 callback = self.callbacks[ps4.host].get(ps4)
