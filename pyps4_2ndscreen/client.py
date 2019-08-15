d36c96fae037a52a45ac0b9b70b0fdd23df5cdfc
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-08-15 04:05:16 -0700

    Use async for client

--- pyps4_2ndscreen/client.py
+++ pyps4_2ndscreen/client.py
@@ -1,178 +1,116 @@
 # -*- coding: utf-8 -*-
-"""Client for PS4."""
+"""Asyncio Client Example for PS4."""
+import asyncio
 import logging
-import threading
-import time
-import queue
+
+from .ddp import async_create_ddp_endpoint
+from .ps4 import Ps4Async
+from .errors import NotReady
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_INTERVAL = 3
-DEFAULT_INTERVAL_KEEP_ALIVE = 30
 
+class Client():
+    """Status Client Class for PS4."""
 
-def _should_poll(last, interval):
-    """Poll timer."""
-    time_since = time.time() - last
-    if time_since > interval:
-        return True
-    return False
-
-
-class PS4Client():
-    """Client for handling background IO."""
-
-    def __init__(self):
+    def __init__(self, loop=None):
         """Init."""
-        self.listeners = {}
-        self.started = []
-
-    def start_all(self):
-        """Start all listeners."""
-        if self.listeners:
-            for listener in self.listeners:
-                listener.start()
-
-    def stop_all(self):
-        """Stop all Listeners."""
-        if self.listeners:
-            for listener in self.listeners:
-                listener.stop.set()
-
-    def remove_all(self):
-        """Remove all listeners."""
-        self.stop_all()
-        self.listeners = {}
-
-    def start_listener(self, ps4):
-        """Start listener thread."""
-        _LOGGER.debug("Starting listener @ %s", ps4._host)  # noqa: pylint: disable=protected-access
-        self.started.append(self.listeners[ps4])
-        self.listeners[ps4].start()
-
-    def stop_listener(self, ps4):
-        """Stop a Listener."""
-        if ps4 in self.listeners:
-            self.listeners[ps4].stop.set()
-            self.started.remove(self.listeners[ps4])
-
-    def remove_listener(self, ps4):
-        """Stop and Remove a Listener."""
-        self.stop_listener(ps4)
-        self.listeners.pop(ps4)
-
-    def schedule_task(self, ps4, task, *args):
-        """Schedule socket task in thread."""
-        active = self.get_active()
-        if active is not None:
-            if active.ps4 != ps4:
-                active.ps4.close()
-        if not ps4.msg_sending:
-            listener = self.listeners[ps4]
-            if args:
-                task = (task, *args)
-            listener.queue.put(task)
-        else:
-            _LOGGER.info("PS4 already has a task in progress")
-
-    def add_ps4(self, ps4):
-        """Add PS4 to listeners."""
-        if ps4 not in self.listeners:
-            self.listeners[ps4] = Listener(client=self, ps4=ps4)
-        else:
-            _LOGGER.warning("Listener already exists")
-
-    def add_callback(self, ps4, callback):
-        """Add Callback for listener."""
-        assert callable(callback) is True, "Callback must be callable"
-        if ps4 in self.listeners:
-            self.listeners[ps4].callbacks.append(callback)
-
-    def get_active(self):
-        """Return logged in PS4."""
-        for listener in self.listeners.values():
-            if listener.ps4.connected:
-                return listener
-        return None
-
-
-class Listener(threading.Thread):
-    """Listener per PS4 to handle sockets."""
-
-    def __init__(self, client, ps4):
-        """Init."""
-        super().__init__()
-        self.client = client
-        self.ps4 = ps4
-        self.interval = DEFAULT_INTERVAL
-        self.interval_keep_alive = DEFAULT_INTERVAL_KEEP_ALIVE
+        self.task = None
         self.last_poll = 0
-        self.last_keep_alive = 0
-        self.callbacks = []
+        self.poll_interval = 0
+        self.ddp_protocol = None
+        self.ps4_devices = []
+        self.callbacks = {}
         self.running = False
-        self._status = None
-        self.queue = queue.Queue()
 
-        self.stop = threading.Event()
-        self.block = threading.Event()
+        if loop is None:
+            self.loop = asyncio.get_event_loop()
 
-    def run(self):
-        """Run Listener."""
-        while not self.stop.is_set():
+    def add_ps4(self, ip_address, creds):
+        """Add PS4 to Client."""
+        ps4 = Ps4Async(ip_address, creds)
+        self.ps4_devices.append(ps4)
+
+    async def init_ps4(self, ps4=None):
+        """Init PS4 devices and DDP Protocol."""
+        if ps4 is not None and self.ddp_protocol is not None:
+            await self.loop.run_in_executor(None, ps4.get_status())
+            ps4.set_protocol(self.ddp_protocol)
+        else:
+            _, self.ddp_protocol = await async_create_ddp_endpoint()
+
+            for device in self.ps4_devices:
+                await self.loop.run_in_executor(None, device.get_status)
+                device.set_protocol(self.ddp_protocol)
+                device.add_callback(self.status_callback)
+
+    def start(self, poll_interval=10):
+        """Start client/loop."""
+        if not self.ps4_devices:
+            return
+
+        self.poll_interval = poll_interval
+        if self.poll_interval < 1:
+            raise RuntimeError(
+                "Polling Interval must be greater than 1 second")
+        try:
+            _LOGGER.info("Starting Client...")
             self.running = True
-            while not self.queue.empty():
-                if not self.block.is_set():
-                    # Block queue.
-                    self.block.set()
-                    item = self.queue.get()
+            self.task = asyncio.ensure_future(self.run_client())
+            self.loop.run_until_complete(self.task)
+        except KeyboardInterrupt:
+            self.stop()
+        finally:
+            _LOGGER.info("Client Stopped")
 
-                    # If args are passed in.
-                    if isinstance(item, tuple):
-                        args = item[1:]
-                        item = item[0]
-                        result = item(*args)
-                    else:
-                        result = item()
+    def stop(self):
+        """Stop Client."""
+        if self.task:
+            _LOGGER.info("Stopping Client...")
+            self.running = False
+            try:
+                self.task.cancel()
+                self.task.exception()
+                self.loop.close()
+            except asyncio.InvalidStateError:
+                pass
 
-                    if item == self.ps4.get_status:
-                        self._check_status(result)
-                        self.last_poll = time.time()
-                    elif item == self.ps4.send_status:
-                        self.last_keep_alive = time.time()
+    def status_callback(self):
+        """Callback for PS4 Status Changed."""
+        _LOGGER.debug("Callback called by DDP Protocol")
+        for ps4 in self.ps4_devices:
+            status = self.ps4.status
+            _LOGGER.info(status)
 
-                    # Reset intervals for status.
-                    else:
-                        self.last_poll = time.time()
-                        self.last_keep_alive = time.time()
+    async def run_client(self):
+        """Running Loop."""
+        # Create DDP transport for status updates.
+        if self.ddp_protocol is None:
+            await self.init_ps4()
+            _LOGGER.debug("Client Started")
 
-                    # Unblock queue.
-                    self.block.clear()
+        if self.running:
+            await self.status_handler()
+        else:
+            self.ddp_protocol.close()
 
-            # Call when queue is empty.
-            # Queue status update.
-            if _should_poll(self.last_poll, self.interval):
-                self.queue.put(self.ps4.get_status, False)
+    async def status_handler(self):
+        """Poll PS4 for status."""
+        if self.loop.time() - self.last_poll > self.poll_interval:
+            polled_hosts = []
 
-            # Queue Keep Alive.
-            if self.ps4.connected:
-                if _should_poll(self.last_keep_alive,
-                                self.interval_keep_alive):
-                    self.queue.put(self.ps4.send_status, False)
+            for ps4 in self.ps4_devices:
+                if ps4.host not in polled_hosts:
+                    _LOGGER.debug("Getting status for: %s", ps4.host)
+                    ps4.get_status()
+                    polled_hosts.append(ps4.host)
 
-        # If stopping.
-        self.queue.join()
-        self.running = False
-        _LOGGER.debug("Stopping Listener @ %s", self.ps4._host)   # noqa: pylint: disable=protected-access
+                if not ps4.connected and not ps4.is_standby and ps4.is_available:
+                    try:
+                        await ps4.async_connect()
+                    except NotReady:
+                        pass
 
-    def _notify_callbacks(self):
-        """Call all callbacks."""
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback()
-
-    def _check_status(self, status):
-        if status != self._status:
-            _LOGGER.debug(
-                "PS4 @ %s: Status changed to %s", self.ps4._host, status)  # noqa: pylint: disable=protected-access
-            self._status = status
-            self._notify_callbacks()
+            self.last_poll = self.loop.time()
+        await asyncio.sleep(1.0)
+        await self.run_client()
