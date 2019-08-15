80cc465f301e5c05036468753272f662376431af
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-08-15 04:04:40 -0700

    Add more commands

--- pyps4_2ndscreen/__main__.py
+++ pyps4_2ndscreen/__main__.py
@@ -6,6 +6,7 @@ import json
 import click
 
 from .helpers import Helper
+from .client import Client
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,19 +14,71 @@ _LOGGER = logging.getLogger(__name__)
 @click.group()
 @click.version_option()
 def cli():
-    """Entry Point for CLI."""
+    """pyps4-2ndscreen CLI.
+
+    Allows for simple commands from terminal."""
     logging.basicConfig(level=logging.INFO)
 
 
+@cli.command(help='Run polling status client. Example: pyps4-2ndscreen client')
+def client():
+    """Run Status Client"""
+    helper = Helper()
+    is_data = helper.check_data('ps4')
+    if not is_data:
+        _input = input(
+            "No Config found. Enter 'Y' to configure: > ")
+        if _input.upper() == 'Y':
+            config_successful = _configure_func()
+            if not config_successful:
+                return
+        else:
+            return
+
+    data = helper.load_files('ps4')
+    status_client = Client()
+    for ip_address in data:
+        creds = data[ip_address]
+        status_client.add_ps4(ip_address, creds)
+    print("Starting Status Client")
+    print("To cancel press 'CTRL + C'.")
+    status_client.start()
+
+
+@cli.command(help='Configure a PS4 console. Example: pyps4-2ndscreen configure')
+def configure():
+    """Configure a PS4 console."""
+    _configure_func()
+
+
+def _configure_func():
+    creds = _credentials_func()
+    if creds is None:
+        return False
+    _search_func()
+    ip_address = input("Enter the IP Address for the PS4 you would like to link: > ")
+
+    linked = _link_func(ip_address, creds)
+    if not linked:
+        return False
+    print("Configuration Successful.")
+    return True
+
+
 @cli.command(help='Search for PS4 devices. Example: pyps4-2ndscreen search')
-def search():
+def search() -> list:
     """Search LAN for PS4's."""
+    _search_func()
+
+
+def _search_func():
     helper = Helper()
     devices = helper.has_devices()
     device_list = [device["host-ip"] for device in devices]
     print("Found {} devices:".format(len(device_list)))
     for ip_address in device_list:
         print(ip_address)
+    return device_list
 
 
 @cli.command(help='Get status of PS4. Example: pyps4-2ndscreen status 192.168.0.1')
@@ -50,6 +103,10 @@ def status(ip_address):
 )
 def credentials():
     """Get and save credentials."""
+    _credentials_func()
+
+
+def _credentials_func():
     from .credential import DEFAULT_DEVICE_NAME
     from .ddp import DDP_PORT
 
@@ -68,17 +125,22 @@ def credentials():
     if creds is not None:
         print("PSN Credentials is: '{}'".format(creds))
         file_name = helper.check_files('credentials')
-        helper.save_files(data, file_name=file_name)
+        helper.save_files(data=data, file_type='credentials', file_name=file_name)
         print("Credentials saved to: {}".format(file_name))
         return creds
     print("No credentials found. Stopped credential service.")
+    return None
 
 
 @cli.command(help='Link PS4. Example: pyps4-2ndscreen link 192.0.0.1 credentials')
 @click.argument('ip_address', required=True)
 @click.argument('credentials', default=None, required=False)
 def link(ip_address, credentials):
-    """Link or register devicee with PS4."""
+    """Link or register device with PS4."""
+    _link_func(ip_address, credentials)
+
+
+def _link_func(ip_address, credentials):
     helper = Helper()
     if credentials is None:
         creds_file = helper.check_files('credentials')
@@ -98,7 +160,7 @@ def link(ip_address, credentials):
         return False
     pin = input(
         "On PS4, Go to settings->Mobile App Connection Settings->"
-        "Add Device and enter pin that is displayed >"
+        "Add Device and enter pin that is displayed: > "
     )
     is_ready, is_login = helper.link(ip_address, credentials, pin)
     if is_ready and is_login:
