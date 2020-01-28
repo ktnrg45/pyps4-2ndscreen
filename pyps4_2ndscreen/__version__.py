6fc0f1182e8db06f0b5eef30e8228a9a90ec2a81
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2020-01-28 01:04:47 -0700

    1.0.6

--- pyps4_2ndscreen/__version__.py
+++ pyps4_2ndscreen/__version__.py
@@ -2,7 +2,7 @@
 """Constants for pyps4-2ndscreen."""
 MAJOR_VERSION = 1
 MINOR_VERSION = 0
-PATCH_VERSION = 5
+PATCH_VERSION = 6
 REQUIRED_PYTHON_VER = (3, 5, 3)
 
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
