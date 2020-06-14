77d362ef872931b6e635dd67efbd801799c273bd
ktnrg45 <ktnrg45@gmail.com>
2020-06-14 14:54:41 -0700

    Update script

--- pyps4_2ndscreen/__version__.py
+++ pyps4_2ndscreen/__version__.py
@@ -7,3 +7,6 @@ REQUIRED_PYTHON_VER = (3, 5, 3)
 
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
+
+if __name__ == "__main__":
+    print(__version__)
