c95472d8b9c38a137d63f212c22855858a8d96dc
ktnrg45 <ktnrg45@gmail.com>
2020-10-17 21:07:32 +0000

    Order results for media art tests

--- tests/.test_media_art_real.py
+++ tests/.test_media_art_real.py
@@ -2,6 +2,7 @@
 import logging
 import asyncio
 import time
+import itertools
 
 import pyps4_2ndscreen.ps4 as ps4
 from pyps4_2ndscreen.errors import PSDataIncomplete
@@ -61,79 +62,111 @@ TEST_CREDS = "Imatest000"
 TEST_PS4 = ps4.Ps4Async(TEST_HOST, TEST_CREDS)
 
 
-async def _get_cover_art(item, search_all):
-    start = time.time()
-    test_index = TEST_LIST.index(item)
-    title = item[0]
-    title_id = item[1]
-    region = item[2]
+class MediaArtTest():
+    def __init__(self):
+        self.results = list(itertools.repeat(None, len(TEST_LIST)))
 
-    try:
-        result_item = await TEST_PS4.async_get_ps_store_data(
-            title, title_id, region, search_all=search_all)
-    except PSDataIncomplete:
-        _LOGGER.error(
-            "PS Data Incomplete: %s, %s, %s", title, title_id, region)
+    def _print_results(self):
+        for index, test_item in enumerate(TEST_LIST):
+            result_item = self.results[index]
+            if index == 0:
+                index = "0 (Search All)"
+            if result_item is None:
+                _LOGGER.info(
+                    "\nResult %s:"
+                    "\n--> Failed"
+                    "\n-------------"
+                    "\nSearch Query:\n--> Title: %s\n--> SKU ID: %s\n--> Region: %s\n",
+                    index,
+                    test_item[0],
+                    test_item[1],
+                    test_item[2],
+                )
+                continue
+
+            _LOGGER.info(
+                "\nResult %s:"
+                "\n--> Title: %s\n--> Cover URL: %s"
+                "\n-------------"
+                "\nSearch Time: %s seconds"
+                "\nSearch Query:\n--> Title: %s\n--> SKU ID: %s\n--> Region: %s\n",
+                index,
+                result_item['name'],
+                result_item['cover'],
+                result_item['elapsed'],
+                test_item[0],
+                test_item[1],
+                test_item[2],
+            )
+
+    async def _get_cover_art(self, item, search_all):
+        """Return result if fail."""
+        start = time.time()
+        test_index = TEST_LIST.index(item)
+        title = item[0]
+        title_id = item[1]
+        region = item[2]
+
+        try:
+            result_item = await TEST_PS4.async_get_ps_store_data(
+                title, title_id, region, search_all=search_all)
+        except PSDataIncomplete:
+            _LOGGER.error(
+                "PS Data Incomplete: %s, %s, %s", title, title_id, region)
+            return item
+
+        try:
+            assert result_item is not None
+            assert title_id in result_item.cover_art
+        except AssertionError:
+            _LOGGER.info("Search Failed: %s, %s, %s\n", title, title_id, region)
+            return item
+
+        if result_item is not None:
+            elapsed = time.time() - start
+            data = {
+                'name': result_item.name,
+                'cover': result_item.cover_art,
+                'elapsed': round(elapsed, 2),
+            }
+            self.results.pop(test_index)
+            self.results.insert(test_index, data)
         return None
 
-    try:
-        assert result_item is not None
-        assert title_id in result_item.cover_art
-    except AssertionError:
-        _LOGGER.info("Search Failed: %s, %s, %s\n", title, title_id, region)
-        return item
+    async def _get_tests(self):
+        tests = []
+        fails = False
+        for item in TEST_LIST:
+            test = self._get_cover_art(item, search_all=False)
+            tests.append(test)
+        tests.pop(0)
+        test_search_all = self._get_cover_art(TEST_LIST[0], search_all=True)
 
-    if result_item is not None:
-        elapsed = time.time() - start
-        _LOGGER.info(
-            "\nResult %s:"
-            "\n--> Title: %s\n--> Cover URL: %s"
-            "\n-------------"
-            "\nSearch Time: %s seconds"
-            "\nSearch Query:\n--> Title: %s\n--> SKU ID: %s\n--> Region: %s\n",
-            test_index,
-            result_item.name,
-            result_item.cover_art,
-            round(elapsed, 2),
-            title,
-            title_id,
-            region,
-        )
-    return None
-
-
-async def _get_tests():
-    tests = []
-    fails = False
-    for item in TEST_LIST:
-        test = _get_cover_art(item, search_all=False)
-        tests.append(test)
-    tests.pop(0)
-    test_search_all = _get_cover_art(TEST_LIST[0], search_all=True)
-
-    results = await asyncio.gather(*tests)
-    for index, item in enumerate(results):
-        if item is not None:
+        results = await asyncio.gather(*tests)
+        _LOGGER.info("\nStarting Search All Test:\n")
+        search_all_result = await test_search_all
+        self._print_results()
+        for index, item in enumerate(results):
+            if item is not None:
+                fails = True
+                _LOGGER.info("Failed: %s", item)
+        if search_all_result is not None:
             fails = True
-            _LOGGER.info("Failed: %s", item)
-    _LOGGER.info("\nStarting Search All Test:\n")
-    search_all_result = await test_search_all
-    if search_all_result is not None:
-        fails = True
-        _LOGGER.info("Search All Test Failed")
-    if not fails:
-        _LOGGER.info("All Tests Passed")
-        return True
-    return False
+            _LOGGER.info("Search All Test Failed")
+        if not fails:
+            _LOGGER.info("All Tests Passed")
+            return True
+        return False
 
 
 def main():
     """Run Tests."""
     _LOGGER.info(
         "\nTest last ran on %s\nVersion: %s\n", time.ctime(), __version__)
+    test_case = MediaArtTest()
     loop = asyncio.get_event_loop()
     start = time.time()
-    success = loop.run_until_complete(_get_tests())
+    success = loop.run_until_complete(test_case._get_tests())
     elapsed = time.time() - start
     _LOGGER.info("All Tests completed in %s seconds", elapsed)
     _LOGGER.info("Success: %s", success)
